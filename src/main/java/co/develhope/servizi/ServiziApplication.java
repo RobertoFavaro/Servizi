45aad733bc8287d953884d2106e30088f5677a6e
RobertoFavaro <robyfavaro04@gmail.com>
2024-04-24 11:53:47 +0200

    initial commit

---
+++ src/main/java/co/develhope/servizi/ServiziApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.servizi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiziApplication.class, args);
+	}
+
+}
